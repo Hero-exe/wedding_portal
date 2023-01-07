e495bbc3d14ce5595c49317fca5ac30ad38f9dc7
Hero-exe <nananakaze@gmail.com>
2023-01-07 18:12:18 +0900

    ヘッダーのコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div class="h-screen w-screen flex justify-center items-center bg-white">
-    <h1 className="text-2xl font-bold text-black">
-      Welcome our wedding
-    </h1>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header msg="Vite + Vue" />
   <button class="text-white bg-blue-500 py-2 px-3">Primary</button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
