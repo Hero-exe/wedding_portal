fb5ca53d57badf2d210de73b1ec2f2ccafd3f202
Hero-exe <nananakaze@gmail.com>
2023-01-29 19:07:09 +0900

    gapiでのアップロード処理を追加

--- src/components/PhotoUpload.vue
+++ src/components/PhotoUpload.vue
@@ -1,10 +1,45 @@
 <script setup>
+  import gapi from 'gapi-client'
+
+  const albumId = ''
+
+  const uploadImage = async (event) => {
+    const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onloadend = async () => {
+        const arrayBuffer = reader.result;
+        const response = await gapi.client.photoslibrary.uploads.create({
+          albumId: albumId,
+          newMediaItem: {
+            description: 'Test image.',
+            simpleMediaItem: {
+              fileName: file.name
+            }
+          },
+          data: arrayBuffer
+        });
+        const uploadToken = response.result.uploadToken;
+        const uploadResponse = await gapi.client.photoslibrary.mediaItems.upload({
+          albumId: albumId,
+          uploadToken: uploadToken,
+          newMediaItem: {
+            description: 'Test image.',
+            simpleMediaItem: {
+              fileName: file.name,
+              uploadToken: uploadToken
+            }
+          }
+        });
+        console.log('Image uploaded successfully: ', uploadResponse)
+      }
+    }
 
 </script>
 
 <template>
   <div class="h-screen w-screen flex justify-center items-center bg-light-snow flex-col">
-    <h1 className="text-2xl font-bold text-black">
+    <h1 className="text-2xl font-bold text-black pb-8">
       PhotoUpload
     </h1>
     <div class="flex items-center justify-center w-full">
@@ -13,7 +48,7 @@
           <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
           <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Touch to upload</span></p>
         </div>
-        <input id="dropzone-file" type="file" class="hidden" />
+        <input id="dropzone-file" type="file" class="hidden" @change="uploadImage" />
       </label>
     </div>
   </div>
