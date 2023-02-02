b71284adfd2ace41b1b6f7f12319c30e16759bc9
Hero-exe <nananakaze@gmail.com>
2023-02-02 18:46:01 +0900

    Uploadのimportをコメントアウト

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Header from './components/Header.vue'
 import SeatingChart from './components/SeatingChart.vue'
 import FoodMenu from './components/FoodMenu.vue'
 import TimeSchedule from './components/TimeSchedule.vue'
-import PhotoUpload from './components/PhotoUpload.vue'
+// import PhotoUpload from './components/PhotoUpload.vue'
 import Greeting from './components/Greeting.vue'
 </script>
 
