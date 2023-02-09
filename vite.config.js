249be53ac28b536fb31e2e7edf988bc07ec7b566
Hero-exe <nananakaze@gmail.com>
2023-02-09 18:25:10 +0900

    configの整形

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader({ defaultImport: 'component' })],
+  plugins: [
+    vue(),
+    svgLoader({ defaultImport: 'component' })
+  ],
 })
