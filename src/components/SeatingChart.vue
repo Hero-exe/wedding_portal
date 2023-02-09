6cebb8911e3b43448263bf806a7cc4dc2e13e6d8
Hero-exe <nananakaze@gmail.com>
2023-02-09 18:25:22 +0900

    高砂を設置

--- src/components/SeatingChart.vue
+++ src/components/SeatingChart.vue
@@ -1,12 +1,13 @@
 <script setup>
-
+import Takasago from '../assets/TakasagoDesign.svg?component'
 </script>
 
 <template>
-    <div class="h-screen w-screen flex justify-center items-center bg-light-yellow">
+    <div class="h-screen w-screen flex justify-center items-center bg-light-yellow flex-col">
     <h1 className="text-2xl font-bold text-black">
       Seating Chart
     </h1>
+    <Takasago class="h-1/5 w-3/4"></Takasago>
   </div>
 </template>
 
