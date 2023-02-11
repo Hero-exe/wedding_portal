21b886d3c740407adc0b1a107cf94437d35bd223
Hero-exe <nananakaze@gmail.com>
2023-02-11 15:50:42 +0900

    テーブルのレイアウトの作成

--- src/components/SeatingChart.vue
+++ src/components/SeatingChart.vue
@@ -4,14 +4,23 @@ import Table from '../components/part/Table.vue'
 </script>
 
 <template>
-  <div class="h-screen w-screen flex justify-center items-center bg-light-yellow flex-col">
-    <h1 class="text-2xl font-bold text-black">
+  <div class="h-max w-screen flex justify-center items-center bg-light-yellow flex-col">
+    <h1 class="text-2xl font-bold text-black mt-7">
       Seating Chart
     </h1>
     <Takasago class="h-1/5 w-3/4"></Takasago>
-    <!--<div class="flex flex-row">-->
-      <Table></Table>
-    <!--</div>-->
+    <div class="flex flex-row">
+      <Table label="A" class="mr-7"></Table>
+      <Table label="B" class="ml-7"></Table>
+    </div>
+    <div class="flex flex-row mt-7">
+      <Table label="C" class="mr-7"></Table>
+      <Table label="D" class="ml-7"></Table>
+    </div>
+    <div class="flex flex-row mt-7 mb-7">
+      <Table label="E" class="mr-7"></Table>
+      <Table label="F" class="ml-7"></Table>
+    </div>
   </div>
 </template>
 
--- src/components/part/Table.vue
+++ src/components/part/Table.vue
@@ -1,10 +1,18 @@
 <script setup>
+  import { defineProps } from 'vue';
+
+  defineProps({
+  label: {
+    type: String,
+    default: "label"
+  }
+})
 </script>
 
 <template>
   <div class="relative">
     <img src="../../assets/png/TableDesign.png" alt="Logo" class="h-32 w-32">
-    <h1 class="text-2xl font-bold text-black top-1/2 left-1/2">A</h1>
+    <h1 class="text-7xl font-bold text-black absolute top-1/2 left-2">{{ label }}</h1>
   </div>
 </template>
 
