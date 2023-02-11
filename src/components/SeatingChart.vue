d1164cee0fac5187dbbdfddba4cef025ccc44fcf
Hero-exe <nananakaze@gmail.com>
2023-02-11 14:25:06 +0900

    Tableをパーツ化

--- src/components/SeatingChart.vue
+++ src/components/SeatingChart.vue
@@ -1,13 +1,17 @@
 <script setup>
-import Takasago from '../assets/TakasagoDesign.svg?component'
+import Takasago from '../assets/svg/TakasagoDesign.svg?component'
+import Table from '../components/part/Table.vue'
 </script>
 
 <template>
-    <div class="h-screen w-screen flex justify-center items-center bg-light-yellow flex-col">
-    <h1 className="text-2xl font-bold text-black">
+  <div class="h-screen w-screen flex justify-center items-center bg-light-yellow flex-col">
+    <h1 class="text-2xl font-bold text-black">
       Seating Chart
     </h1>
     <Takasago class="h-1/5 w-3/4"></Takasago>
+    <!--<div class="flex flex-row">-->
+      <Table></Table>
+    <!--</div>-->
   </div>
 </template>
 
---
+++ src/components/part/Table.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="relative">
+    <img src="../../assets/png/TableDesign.png" alt="Logo" class="h-32 w-32">
+    <h1 class="text-2xl font-bold text-black top-1/2 left-1/2">A</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
