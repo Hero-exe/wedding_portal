12fca1f2f6d6a6f8c47a33739d28018594b311bf
Hero-exe <nananakaze@gmail.com>
2023-02-15 19:04:36 +0900

    席次表暫定版

---
+++ src/components/part/SeatName.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { defineProps } from 'vue';
+
+  defineProps({
+  name1: {
+    type: String
+  },
+  name2: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <p class="text-black text-xl w-40">{{ name1 }}</p>
+  <p class="text-black text-xl w-40">{{ name2 }}</p>
+</template>
+
+<style scoped>
+
+</style>
