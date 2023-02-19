36bcb0a5f7c9b27bcad8a994e8f41a90448b352e
Hero-exe <nananakaze@gmail.com>
2023-02-19 17:13:30 +0900

    Github Pagesの設定を追加

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'wedding_portal',
   plugins: [
     vue(),
     svgLoader({ defaultImport: 'component' })
