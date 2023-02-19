b710b021fb4980083056148cdde7f7081791a946
Hero-exe <nananakaze@gmail.com>
2023-02-19 15:45:12 +0900

    Greetingの取り込み

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -4,9 +4,7 @@
 
 <template>
     <div class="h-screen w-screen flex justify-center items-center bg-white">
-    <h1 className="text-2xl font-bold text-black">
-      Greeting
-    </h1>
+      <img src="../assets/png/Greeting.png" alt="Logo" class="h-5/6">
   </div>
 </template>
 
